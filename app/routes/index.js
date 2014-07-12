bf454f723eca2da6f9381843f10c7a02c6720969
eaortiz <eaortiz@stanford.edu>
2014-07-12 15:49:34 -0700

    path implemented

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET /independent/dependent */
+router.get('/:independent/:dependent', function(req, res) {
+	file = req.params['independent'] + '-view.jade';
+	console.log(file);
+	res.render(file, {dependent: req.params['dependent']});
+});
+
 module.exports = router;
